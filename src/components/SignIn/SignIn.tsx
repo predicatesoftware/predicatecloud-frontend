2802414bf37d12f3c4c2325926221062e40682bf
Artyom Khudyakov <47335768+HardenedHunter@users.noreply.github.com>
2020-06-24 17:27:39 +0300

    Fixed font and image problems, added Twitter link

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -3,19 +3,14 @@ import { RouteComponentProps, Redirect } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm';
 import authService from '../../api/services/authService';
 import styles from './styles.module.scss';
+import Image from './../Image/index';
 
 export default ({ history }: RouteComponentProps) => {
   return (
     <>
       <div className={styles.formContainer}>
         <div className={styles.formImage}>
-          <img
-            src="images/PredicateLogo.png"
-            alt="Predicate Cloud"
-            title="Predicate Cloud"
-            width="150px"
-            height="150px"
-          />
+          <Image src="PredicateLogo150.png" alt="Predicate Cloud" />
         </div>
         <LoginForm history={history} />
       </div>
